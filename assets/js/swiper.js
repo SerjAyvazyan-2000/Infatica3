853c8e47bc75683de1ea49771a28028acf330f5f
SerjAyvazyan-2000 <ayvazyanserj3@gmail.com>
2024-12-12 12:17:44 +0400

    new

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,7 +1,7 @@
 const swiper = new Swiper('.proxy-swiper', {
     slidesPerView: 4,
     spaceBetween: 32,
-    loop: true,
+    // loop: true,
     pagination: {
         el: '.services-pagination',
         clickable: true,
@@ -36,7 +36,7 @@ const swiper = new Swiper('.proxy-swiper', {
         },
 
         760: {
-            slidesPerView: 2.6,
+            slidesPerView: 'auto',
             spaceBetween: 10
 
         },
